d0aa551a46a51a09469dab3568e853cf9afb4d9d
mathiasjk1 <mathiaskaltoft2@gmail.com>
2017-02-28 15:33:06 +0100

    Release
    Tilføjet et par kommentarer til koden.

--- js/index1.js
+++ js/index1.js
@@ -1,5 +1,4 @@
 //Adressen på Bluetooth Mate, Password er 1234
-//var macAddress = "00:06:66:07:B1:B7";
 var macAddress = "00:06:66:7D:98:60";
 function onLoad(){
 	document.addEventListener("deviceready", onDeviceReady, false);
